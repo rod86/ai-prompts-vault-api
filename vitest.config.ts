73e4752eb3ff1ac212486705a10bf87a64c07ad2
Sergi Rodriguez <sergi86@gmail.com>
2026-07-16 17:21:15 +0200

    setup test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             reportsDirectory: 'coverage',
+            reporter: ['text', 'html', 'lcov', 'json-summary'],
+            thresholds: {
+                statements: 80, // executable statements (assignment, return, throw,...)
+                branches: 80, // decisions paths taken (if, switch,...)
+                functions: 80, // functions called once
+                lines: 80, // source line that ran once
+            },
         },
         projects: [
             {
